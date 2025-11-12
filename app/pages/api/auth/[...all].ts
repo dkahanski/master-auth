8d3e5cfd6640e0adebbf0faa05529333e346cd70
Dmitry M. Kaganski <d.kagansky@gmail.com>
2025-11-12 12:24:14 +0300

    Updated better auth config

--- app/pages/api/auth/[...all].ts
+++
@@ -1,6 +0,0 @@
-import { toNodeHandler } from 'better-auth/node';
-import { auth } from '@/app/lib/auth';
-
-// Disallow body parsing, we will parse it manually
-export const config = { api: { bodyParser: false } };
-export default toNodeHandler(auth.handler);
